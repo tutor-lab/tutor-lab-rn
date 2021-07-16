c4707911f3d245a56818cf5e9bb987436e7a2993
YunJu Lee <ollabu3@gmail.com>
2021-07-16 23:01:41 +0900

    로그인 스택 구조

---
+++ src/screens/login/FindPwdScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const FindPwdScreen = () => {
+  return (
+    <View>
+      <Text>FindPwdScreen</Text>
+    </View>
+  );
+};
+
+export default FindPwdScreen;
