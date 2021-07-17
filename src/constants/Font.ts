cd86ad94cfed678d41dfe666ac9ddbfe46ee6368
YunJu Lee <ollabu3@gmail.com>
2021-07-17 16:23:21 +0900

    폰트 적용 - SpoqaHasSansNeo

---
+++ src/constants/Font.ts
@@ -0,0 +1,8 @@
+const Font = {
+  Bold: 'SpoqaHanSansNeo-Bold',
+  light: 'SpoqaHanSansNeo-Light',
+  medium: 'SpoqaHanSansNeo-Medium',
+  regular: 'SpoqaHanSansNeo-Regular',
+  thin: 'SpoqaHanSansNeo-Thin',
+};
+export default Font;
