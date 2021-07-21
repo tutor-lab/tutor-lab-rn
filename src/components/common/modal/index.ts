346c071795683b91002983c430662df8090c0965
YunJu Lee <ollabu3@gmail.com>
2021-07-21 18:37:01 +0900

    cunstom modal component

---
+++ src/components/common/modal/index.ts
@@ -0,0 +1,15 @@
+import Container from './Container';
+import Header from './Header';
+import Title from './Title';
+import Description from './Description';
+import OneBtn from './OneBtn';
+
+const CustomModal = {
+  Container,
+  Header,
+  Title,
+  Description,
+  OneBtn,
+};
+
+export default CustomModal;
