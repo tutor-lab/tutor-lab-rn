22a8f325748313f2ab33c279131f3b9b1bf5d13f
YunJu Lee <ollabu3@gmail.com>
2021-07-23 19:04:20 +0900

    main stack 추가 - Main -> bottom tab navigator(FreeBoard, Lecture, MyPage)

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -1,5 +1,13 @@
 export const logo = require('../assets/icons/logo.png');
+export const lecture = require('../assets/icons/lecture.svg');
+export const free_board = require('../assets/icons/free_board.svg');
+export const my_page = require('../assets/icons/my_page.svg');
+export const heart = require('../assets/icons/heart.svg');
 
 export default {
   logo,
+  lecture,
+  free_board,
+  my_page,
+  heart,
 };
