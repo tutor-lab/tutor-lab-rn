4f79f136cc425328c5aa551bd27d233b88b7ea52
YunJu Lee <ollabu3@gmail.com>
2021-08-11 01:26:58 +0900

    main custom bottomtab 추가

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -1,25 +1,43 @@
 export const logo = require('../assets/icons/logo.png');
-export const lecture = require('../assets/icons/lecture.svg');
-export const free_board = require('../assets/icons/free_board.svg');
-export const my_page = require('../assets/icons/my_page.svg');
-export const heart = require('../assets/icons/heart.svg');
 export const category_menu = require('../assets/icons/category_menu.svg');
 export const category_arrow = require('../assets/icons/category_arrow.svg');
-export const main_header = require('../assets/icons/main_header.svg');
 export const lecture_back_arrow = require('../assets/icons/lecture_back_arrow.svg');
 export const logo_icon = require('../assets/icons/logo_icon.svg');
 export const logo_text = require('../assets/icons/logo_text.svg');
 
+export const heart = require('../assets/icons/heart.svg');
+export const alert = require('../assets/icons/alert.svg');
+export const refresh = require('../assets/icons/refresh.svg');
+export const search = require('../assets/icons/search.svg');
+export const star = require('../assets/icons/star.svg');
+
+export const home_selected = require('../assets/icons/home_selected.svg');
+export const home_unselected = require('../assets/icons/home_unselected.svg');
+export const free_board_selected = require('../assets/icons/free_board_selected.svg');
+export const free_board_unselected = require('../assets/icons/free_board_unselected.svg');
+export const chat_selected = require('../assets/icons/chat_selected.svg');
+export const chat_unselected = require('../assets/icons/chat_unselected.svg');
+export const my_page_selected = require('../assets/icons/my_page_selected.svg');
+export const my_page_unselected = require('../assets/icons/my_page_unselected.svg');
+
 export default {
   logo,
-  lecture,
-  free_board,
-  my_page,
   heart,
   category_menu,
   category_arrow,
-  main_header,
   lecture_back_arrow,
   logo_icon,
   logo_text,
+  alert,
+  refresh,
+  search,
+  star,
+  home_selected,
+  home_unselected,
+  free_board_selected,
+  free_board_unselected,
+  chat_selected,
+  chat_unselected,
+  my_page_selected,
+  my_page_unselected,
 };
---
+++ src/navigation/MyPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import {View, Text} from 'react-native';
+
+const MyPage = () => {
+  return (
+    <View>
+      <Text>마이페이지</Text>
+    </View>
+  );
+};
+
+export default MyPage;
