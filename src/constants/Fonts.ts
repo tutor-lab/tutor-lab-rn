e4af79e19d22ce815611cc80d3fde97cc1c21330
YunJu Lee <ollabu3@gmail.com>
2021-08-13 01:06:24 +0900

    폰트 변경

--- src/constants/Fonts.ts
+++ src/constants/Fonts.ts
@@ -1,9 +1,15 @@
 const fonts = {
-  Bold: 'SpoqaHanSansNeo-Bold',
-  light: 'SpoqaHanSansNeo-Light',
-  medium: 'SpoqaHanSansNeo-Medium',
-  regular: 'SpoqaHanSansNeo-Regular',
-  thin: 'SpoqaHanSansNeo-Thin',
+  Bold: 'NotoSansKR-Bold',
+  light: 'NotoSansKR-Light',
+  medium: 'NotoSansKR-Medium',
+  regular: 'NotoSansKR-Regular',
+  thin: 'NotoSansKR-Thin',
+  '100': {fontFamily: 'NotoSansKR-Thin', includeFontPadding: false},
+  '300': {fontFamily: 'NotoSansKR-Light', includeFontPadding: false},
+  '400': {fontFamily: 'NotoSansKR-Regular', includeFontPadding: false},
+  '500': {fontFamily: 'NotoSansKR-Medium', includeFontPadding: false},
+  '700': {fontFamily: 'NotoSansKR-Bold', includeFontPadding: false},
+  '900': {fontFamily: 'NotoSansKR-Black', includeFontPadding: false},
 };
 
 export default fonts;
