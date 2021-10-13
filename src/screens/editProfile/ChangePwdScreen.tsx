f3a83af289ec2d18c94439a54736ace02b573da4
ollabu3 <ollabu3@gmail.com>
2021-10-13 16:39:35 +0900

    비밀번호 변경 기능 추가 - 1. 비밀번호 확인(api아직 x, 생성시 연결 필요) 2. 새 비밀번호 양식 확인3. 새 비밀번호 일치 확인

--- src/screens/editProfile/ChangePwdScreen.tsx
+++ src/screens/editProfile/ChangePwdScreen.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'react-native-gesture-handler';
-import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+import {SafeAreaView, View, Text, StyleSheet, Keyboard} from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 import {
@@ -10,11 +10,39 @@ import {
   TextInputLabel,
   TextInput,
   Button,
+  ErrorText,
 } from '../../components/common';
 
-import {width, fonts, colors} from '../../constants';
+import {Modals} from '../../components/editProfile';
+import {width, fonts, colors, utils} from '../../constants';
 
 const ChangePwdScreen = ({navigation}: any) => {
+  const [pwd, setPwd] = useState({text: '', error: ''});
+  const [newPwd, setNewPwd] = useState({text: '', error: ''});
+  const [retypeNewPwd, setRetypeNewPwd] = useState({text: '', error: ''});
+  const [modal, setModal] = useState(false);
+
+  const sendChangePwd = () => {
+    Keyboard.dismiss();
+    if (
+      true
+      // 1. 비밀번호 확인하기 (api 아직 없음)
+    ) {
+      setPwd({text: '', error: '비밀번호가 일치하지 않습니다.'});
+    } else if (!utils.pwdValidator(newPwd.text)) {
+      //  2. 비밀번호 양식 틀리면
+      setNewPwd({text: '', error: '비밀번호는 6-14자 이내 이어야 합니다.'});
+      setRetypeNewPwd({text: '', error: ''});
+    } else if (!utils.pwdTypoCheck(newPwd.text, retypeNewPwd.text)) {
+      // 3. 새 비밀번호 일치하지 않으면
+      setRetypeNewPwd({text: '', error: '새 비밀번호가 일치하지 않습니다.'});
+    } else {
+      // 다 일치하면
+      setModal(true);
+      // 토큰 변경하고
+    }
+  };
+
   return (
     <SafeAreaView style={Commonstyles.container}>
       <Header.Basic title={'비밀번호 변경'} navigation={navigation} />
@@ -30,8 +58,14 @@ const ChangePwdScreen = ({navigation}: any) => {
           <View style={Commonstyles.textInputContainer}>
             <TextInputLabel title={'현재 비밀번호'} />
             <View style={Commonstyles.textInputWrapper}>
-              <TextInput placeholder={'현재 비밀번호'} secureTextEntry />
+              <TextInput
+                placeholder={'현재 비밀번호'}
+                secureTextEntry
+                value={pwd.text}
+                onChangeText={(e: string) => setPwd({...pwd, text: e})}
+              />
             </View>
+            {pwd.error.length !== 0 && <ErrorText text={pwd.error} />}
           </View>
           <View style={Commonstyles.textInputContainer}>
             <TextInputLabel title={'새 비밀번호'} />
@@ -39,18 +73,47 @@ const ChangePwdScreen = ({navigation}: any) => {
               <TextInput
                 placeholder={'새 비밀번호 (6-14자 이내)'}
                 secureTextEntry
+                value={newPwd.text}
+                onChangeText={(e: string) => setNewPwd({...newPwd, text: e})}
               />
             </View>
+            {newPwd.error.length !== 0 && <ErrorText text={newPwd.error} />}
           </View>
           <View style={Commonstyles.textInputContainer}>
             <TextInputLabel title={'새 비밀번호 확인'} />
             <View style={Commonstyles.textInputWrapper}>
-              <TextInput placeholder={'새 비밀번호 확인'} secureTextEntry />
+              <TextInput
+                placeholder={'새 비밀번호 확인'}
+                secureTextEntry
+                value={retypeNewPwd.text}
+                onChangeText={(e: string) =>
+                  setRetypeNewPwd({...retypeNewPwd, text: e})
+                }
+              />
             </View>
+            {retypeNewPwd.error.length !== 0 && (
+              <ErrorText text={retypeNewPwd.error} />
+            )}
           </View>
         </View>
       </KeyboardAwareScrollView>
-      <Button.Button_Bottom title={'비밀번호 변경'} onPress={() => ''} />
+      <Button.Button_Bottom
+        title={'비밀번호 변경'}
+        onPress={() => sendChangePwd()}
+      />
+      {modal && (
+        <Modals.Container visible={modal} setVisible={setModal}>
+          <Modals.Title text={'비밀번호 변경'} />
+          <Modals.Description text={'비밀번호가 변경되었습니다.'} />
+          <Modals.OneBtn
+            text={'확인'}
+            onPress={() => {
+              setModal(false);
+              navigation.goBack();
+            }}
+          />
+        </Modals.Container>
+      )}
     </SafeAreaView>
   );
 };
